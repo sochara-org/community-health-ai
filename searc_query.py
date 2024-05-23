7404ebff24cf0b626534213ce58bed8647820398
aparna <aparna.n.a.23@gmail.com>
2024-05-23 11:16:25 +0530

    searc_query.py

---
+++ searc_query.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import functions
+import warnings
+
+# Suppress the specific FutureWarning from Hugging Face Hub
+
+
+
+app = Flask(__name__)
+
+warnings.filterwarnings("ignore", category=FutureWarning, message="`resume_download` is deprecated*")
+conversation_history = []
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    structured_sentence = None
+    if request.method == 'POST':
+        query_text = request.form['query']
+        structured_sentence, pdf_filename = functions.process_query_clickhouse(query_text)
+        conversation_history.append((query_text, structured_sentence, pdf_filename))
+    return render_template('index.html', conversation_history=conversation_history)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
