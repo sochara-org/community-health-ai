5b6461add22748652621c65ab418787f3b8b8637
aparna <aparna.n.a.23@gmail.com>
2024-06-17 12:21:29 +0530

    Updated functions.py with additional modifications

--- functions.py
+++ functions.py
@@ -1,30 +1,29 @@
-from clickhouse_driver import Client
-from transformers import AutoTokenizer, AutoModel
-from scipy.spatial.distance import cosine
-import torch
-from nltk.tokenize import word_tokenize
-import openai
-from transformers import LlamaTokenizer, LlamaForCausalLM
-import torch
-import sentencepiece
-from transformers import AutoTokenizer, LlamaForCausalLM
-from transformers import AutoModelForCausalLM
 import os
+import re
+import torch
+import urllib.parse
+import openai
+from nltk.tokenize import word_tokenize
+from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
+from clickhouse_driver import Client
+from scipy.spatial.distance import cosine
 from dotenv import load_dotenv
+from stop_words import stop_words
+
+load_dotenv()
+archive_base_url = os.getenv('ARCHIVE_BASE_URL')
 
 print("Current Working Directory:", os.getcwd())
 
 if 'OPENAI_API_KEY' in os.environ:
     del os.environ['OPENAI_API_KEY']
 
-dotenv_path = 'a.env' 
+dotenv_path = 'a.env'
 
 if not os.path.exists(dotenv_path):
     raise FileNotFoundError(f"The .env file at path '{dotenv_path}' does not exist.")
 
-
 load_dotenv(dotenv_path)
-
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
@@ -43,7 +42,6 @@ def initialize_tokenizer_and_model():
     return tokenizer, model
 
 def initialize_llama_model():
-
     llama_tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
     llama_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
     return llama_tokenizer, llama_model
@@ -60,67 +58,57 @@ def generate_embeddings(tokenizer, model, query):
     except Exception as e:
         print("An error occurred while converting query to embeddings:", e)
         return None
-
-def jaccard_similarity(set1, set2):
-    intersection = len(set1.intersection(set2))
-    union = len(set1.union(set2))
-    return intersection / union if union != 0 else 0
-
-def rank_sections(client, question_embedding, question_tokens):
-    try:
-        sections = client.execute("SELECT b_chunks.chunk_text, b_chunks.embeddings, b_table.original_filename FROM b_chunks JOIN b_table ON b_chunks.summary_id = b_table.id")
-        if not sections:
-            print("No sections retrieved from the database.")
-            return None
-        ranked_sections = []
-        question_token_set = set(question_tokens)
-
-        for section in sections:
-            if len(section) < 3:
-                print("Invalid section format:", section)
-                continue
-
-            chunk_text = section[0]
-            if isinstance(chunk_text, bytes):
-                try:
-                    chunk_text = chunk_text.decode('utf-8')
-                except UnicodeDecodeError:
-                    chunk_text = chunk_text.decode('utf-8', errors='replace')
-
-            embeddings_list = section[1]
-            original_filename = section[2]
-
-            if not isinstance(embeddings_list, list):
-                print("Embeddings are not in the expected list format.")
-                continue
-
-            if len(embeddings_list) != len(question_embedding):
-                print("Dimension mismatch between query and chunk embeddings.")
-                continue
-
-            embeddings_tensor = torch.tensor(embeddings_list)
-            question_embedding_tensor = torch.tensor(question_embedding)
-            cosine_similarity = torch.nn.functional.cosine_similarity(question_embedding_tensor, embeddings_tensor, dim=0)
-
-            chunk_tokens = set(word_tokenize(chunk_text.lower()))
-            jaccard_similarity_score = jaccard_similarity(question_token_set, chunk_tokens)
-
-            combined_similarity = (0.7 * cosine_similarity.item()) + (0.3 * jaccard_similarity_score)
-
-            ranked_sections.append((chunk_text, combined_similarity, original_filename))
-
-        ranked_sections.sort(key=lambda x: x[1], reverse=True)
-        return ranked_sections
-
-    except Exception as e:
-        print("An error occurred while ranking sections:", e)
-        return None
     
+def extract_important_words(query_text):
+    words = re.findall(r'\b\w+\b', query_text.lower())
+    important_words = [word for word in words if word not in stop_words]
+    return important_words
+
+
+def get_surrounding_chunks(client, id, summary_id, window_size=2):
+    surrounding_chunks_query = f"""
+    SELECT chunk_text
+    FROM abc_chunks
+    WHERE summary_id = '{summary_id}' AND
+          id >= {id - window_size} AND
+          id <= {id + window_size}
+    ORDER BY id
+    """
+    surrounding_chunks = client.execute(surrounding_chunks_query)
+    full_context = ' '.join([chunk[0] for chunk in surrounding_chunks])
+    return full_context
+
+def get_original_filename(client, summary_id):
+    # Execute the query to get the original filename
+    query = f"SELECT original_filename FROM abc_table WHERE id = '{summary_id}'"
+    result = client.execute(query)
+
+    if result:
+        original_filename = result[0][0]
+
+        # Process the filename to remove 'None' prefix and strip whitespace
+        original_filename = original_filename.split('None', 1)[-1].strip()
+
+        # Extract the filename without the extension
+        filename_without_ext = os.path.splitext(original_filename)[0]
+
+        # Parse the URL to extract the filename
+        parsed_url = urllib.parse.urlparse(filename_without_ext)
+        filename = parsed_url.path.split('/')[-1]
+
+        # Construct the full file URL
+        file_url = f"{archive_base_url}{filename}"
+
+        return file_url
+
+    return None
+
+
 def cosine_similarity(client, question_embedding):
     try:
         question_embedding_str = ','.join(map(str, question_embedding))
         query = f"""
-        SELECT chunk_text, original_filename,
+        SELECT chunk_text, summary_id,
                (dotProduct(embeddings, [{question_embedding_str}]) / 
                 (sqrt(dotProduct(embeddings, embeddings)) * sqrt(dotProduct([{question_embedding_str}], [{question_embedding_str}]))) ) AS cosine_similarity
         FROM b_chunks
@@ -132,7 +120,8 @@ def cosine_similarity(client, question_embedding):
         if not sections:
             print("No sections retrieved from the database.")
             return None, None
-        chunk_text, original_filename = sections[0][:2]
+        chunk_text, summary_id = sections[0][:2]
+        original_filename = get_original_filename(client, summary_id)
         return chunk_text, original_filename
     except Exception as e:
         print("An error occurred during vector search in chunks:", e)
@@ -142,7 +131,7 @@ def vector_search_cosine_distance(client, question_embedding):
     try:
         question_embedding_str = ','.join(map(str, question_embedding))
         query = f"""
-        SELECT chunk_text, original_filename,
+        SELECT chunk_text, summary_id,
                1 - (dotProduct(embeddings, [{question_embedding_str}]) / 
                     (sqrt(dotProduct(embeddings, embeddings)) * sqrt(dotProduct([{question_embedding_str}], [{question_embedding_str}])))
                ) AS cosine_distance
@@ -155,7 +144,8 @@ def vector_search_cosine_distance(client, question_embedding):
         if not sections:
             print("No sections retrieved from the database.")
             return None, None
-        chunk_text, original_filename = sections[0][:2]
+        chunk_text, summary_id = sections[0][:2]
+        original_filename = get_original_filename(client, summary_id)
         return chunk_text, original_filename
     except Exception as e:
         print("An error occurred during vector search in chunks:", e)
@@ -163,11 +153,11 @@ def vector_search_cosine_distance(client, question_embedding):
 
 def ann_search(client, query_embedding, window_size=2):
     try:
-        query_embedding_str = ','.join(map(str, query_embedding))
+        question_embedding_str = ','.join(map(str, query_embedding))
         query = f"""
-        SELECT id, chunk_text, original_filename, summary_id, embeddings,
-               (dotProduct(embeddings, [{query_embedding_str}]) /
-               (sqrt(dotProduct(embeddings, embeddings)) * sqrt(dotProduct([{query_embedding_str}], [{query_embedding_str}])))
+        SELECT id, chunk_text, summary_id, embeddings,
+               (dotProduct(embeddings, [{question_embedding_str}]) /
+               (sqrt(dotProduct(embeddings, embeddings)) * sqrt(dotProduct([{question_embedding_str}], [{question_embedding_str}])))
                ) AS cosine_similarity
         FROM abc_chunks
         JOIN abc_table ON abc_chunks.summary_id = abc_table.id
@@ -179,30 +169,23 @@ def ann_search(client, query_embedding, window_size=2):
             print("No sections retrieved from the database.")
             return None, None
         
-        id, chunk_text, original_filename, summary_id, embeddings, cosine_similarity = sections[0]
+        id, chunk_text, summary_id, embeddings, cosine_similarity = sections[0]
 
         # Retrieve surrounding chunks based on the nearest chunk's id
-        surrounding_chunks_query = f"""
-        SELECT chunk_text
-        FROM abc_chunks
-        WHERE summary_id = '{summary_id}' AND
-              id >= {id - window_size} AND
-              id <= {id + window_size}
-        ORDER BY id
-        """
-        surrounding_chunks = client.execute(surrounding_chunks_query)
-        full_context = ' '.join([chunk[0] for chunk in surrounding_chunks])
+        full_context = get_surrounding_chunks(client, id, summary_id, window_size)
+        original_filename = get_original_filename(client, summary_id)
         return full_context, original_filename
     except Exception as e:
         print("An error occurred during vector search in chunks:", e)
         return None, None
 
 
+
 def euclidean_search(client, question_embedding):
     try:
         question_embedding_str = ','.join(map(str, question_embedding))
         query = f"""
-        SELECT chunk_text, original_filename,
+        SELECT chunk_text, summary_id,
                LpDistance(embeddings, [{question_embedding_str}], 2) AS euclidean_distance
         FROM b_chunks
         JOIN b_table ON b_chunks.summary_id = b_table.id
@@ -213,12 +196,51 @@ def euclidean_search(client, question_embedding):
         if not sections:
             print("No sections retrieved from the database.")
             return None, None
-        chunk_text, original_filename = sections[0][:2]
+        chunk_text, summary_id = sections[0][:2]
+        original_filename = get_original_filename(client, summary_id)
         return chunk_text, original_filename
     except Exception as e:
         print("An error occurred during vector search in chunks:", e)
         return None, None
 
+
+def query_clickhouse_word_with_multi_stage(client, important_words, query_embedding):
+
+    tokenizer, model = initialize_tokenizer_and_model()
+    query_embedding_str = ','.join(map(str, query_embedding))
+
+    # Stage 1: Retrieve potentially relevant chunks based on keyword matching
+    word_query = '%' + '%'.join(important_words) + '%'
+    keyword_matching_query = f"""
+    SELECT id, chunk_text, summary_id, embeddings
+    FROM abc_chunks
+    WHERE lower(chunk_text) LIKE lower('{word_query}')
+    """
+    matched_chunks = client.execute(keyword_matching_query)
+
+    if matched_chunks:
+        # Stage 2: Rank or re-rank the matched chunks using semantic similarity
+        ranked_chunks_query = f"""
+        SELECT c.id, c.chunk_text, c.summary_id, c.embeddings,
+               (dotProduct(c.embeddings, [{query_embedding_str}]) /
+                (sqrt(dotProduct(c.embeddings, c.embeddings)) * sqrt(dotProduct([{query_embedding_str}], [{query_embedding_str}]))))
+               AS cosine_similarity
+        FROM (
+        {keyword_matching_query}
+        ) AS c
+        ORDER BY cosine_similarity DESC
+        LIMIT 1
+        """
+        ranked_chunks = client.execute(ranked_chunks_query)
+
+        if ranked_chunks:
+            id, chunk_text, summary_id, embeddings, cosine_similarity = ranked_chunks[0]
+            full_context = get_surrounding_chunks(client, id, summary_id)
+            original_filename = get_original_filename(client, summary_id)
+            return full_context, original_filename
+
+ 
+    return ann_search(client, query_embedding)
 
 def structure_sentence_with_llama(query, chunk_text, llama_tokenizer, llama_model):
     try:
@@ -252,7 +274,7 @@ def structure_sentence(query, chunk_text):
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
-    
+
 def structure_chunk_text(query, chunk_text):
     try:
         messages = [
@@ -275,24 +297,12 @@ def structure_chunk_text(query, chunk_text):
         print(f"An error occurred: {e}")
         return None
 
-def process_query(query_text):
-    tokenizer, model = initialize_tokenizer_and_model()
-    client = initialize_clickhouse_connection()
-    question_embedding = generate_embeddings(tokenizer, model, query_text)
-    if question_embedding is not None:
-        question_tokens = word_tokenize(query_text.lower())
-        ranked_sections = rank_sections(client, question_embedding, question_tokens)
-        if ranked_sections:
-            nearest_chunk, similarity_score, pdf_filename = ranked_sections[0]
-            structured_sentence = structure_sentence(query_text, nearest_chunk)
-            return structured_sentence, pdf_filename
-    return None, None
 
 def process_query_clickhouse(query_text, search_method='ann_search'):
     tokenizer, model = initialize_tokenizer_and_model()
     client = initialize_clickhouse_connection()
     question_embedding = generate_embeddings(tokenizer, model, query_text)
-    
+
     if question_embedding is not None:
 
         search_methods = {
@@ -301,7 +311,7 @@ def process_query_clickhouse(query_text, search_method='ann_search'):
             'ann_search': ann_search,
             'euclidean_search': euclidean_search
         }
-        
+
         if search_method in search_methods:
             search_function = search_methods[search_method]
             chunk_text, pdf_filename = search_function(client, question_embedding)
@@ -311,4 +321,17 @@ def process_query_clickhouse(query_text, search_method='ann_search'):
             print(f"Search method '{search_method}' is not valid.")
     return None, None
 
+def process_query_clickhouse_word(query_text):
+    tokenizer, model = initialize_tokenizer_and_model()
+    client = initialize_clickhouse_connection()
+    important_words = extract_important_words(query_text)
 
+    if important_words:
+        query_embedding = generate_embeddings(tokenizer, model, query_text)
+        closest_chunks = query_clickhouse_word_with_multi_stage(client, important_words, query_embedding)
+        if closest_chunks:
+            full_context, original_filename = closest_chunks
+            return full_context, original_filename
+
+    print("No relevant chunk found. Performing ann_search as fallback.")
+    return ann_search(client, query_embedding)
