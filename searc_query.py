e144ec31191fba6584051aedf9534999cec4d9a4
aparna <aparna.n.a.23@gmail.com>
2024-06-24 11:03:20 +0530

    Update searc_query.py with additional modifications

--- searc_query.py
+++ searc_query.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 import functions
 import warnings
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
 
 warnings.filterwarnings("ignore", category=FutureWarning, message="`resume_download` is deprecated*")
 conversation_history = []
@@ -15,20 +18,27 @@ def index():
             return jsonify({"bot_reply": "Please enter a valid query."}), 400
 
         try:
-            structured_sentences, pdf_filenames = functions.process_query_clickhouse_pdf(query_text)
+            structured_sentences, pdf_filenames, pdf_descriptions = functions.process_query_clickhouse_pdf(query_text)
         except Exception as e:
             return jsonify({"bot_reply": f"An error occurred: {str(e)}"}), 500
 
         if not structured_sentences:
             structured_sentence = "I'm sorry, I couldn't find a relevant response for your query."
             pdf_filenames = []
+            pdf_descriptions = []
         else:
             structured_sentence = structured_sentences[0]
 
-        conversation_history.append((query_text, structured_sentence, pdf_filenames))
-        return jsonify({"bot_reply": structured_sentence, "pdf_urls": pdf_filenames})
+        print("PDF Descriptions:", pdf_descriptions)
+        conversation_history.append((query_text, structured_sentence, pdf_filenames, pdf_descriptions))
+        return jsonify({
+            "bot_reply": structured_sentence, 
+            "pdf_urls": pdf_filenames, 
+            "pdf_descriptions": pdf_descriptions
+        })
 
-    return render_template('index.html', conversation_history=conversation_history)
+    return render_template('index.html',zip=zip, conversation_history=conversation_history)
+
 
 @app.route('/about')
 def about():
